f0539ccb325a59573613d33de83785cd747702d8
Dart2112 <dart2112@lapismc.net>
2023-11-28 13:40:30 +1030

    Remove old docs

--- docs/package-search-index.js
+++
@@ -1,4 +0,0 @@
-packageSearchIndex = [{
-    "l": "All Packages",
-    "url": "allpackages-index.html"
-}, {"l": "net.lapismc.lapischat.api"}, {"l": "net.lapismc.lapischat.events"}, {"l": "net.lapismc.lapischat.framework"}]
